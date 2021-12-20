51dc625f45701e8051bddde23c0dab00db01170a
Karen <krufino@itech-sac.com>
2021-12-20 16:25:36 -0500

    estructura del SPA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
-
+import { changeView} from './view controler/router.js'
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const init = () =>{
+changeView(window.location.hash); //para que no solo se muestre la vista cuando cambia sino tambien cuando se recarga
+window.addEventListener('hashchange', () => changeView(window.location.hash));
+}
+window.addEventListener('load', init);
