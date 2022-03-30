ced2daae4a375d411db2837f0812461d511642e1
kalyzca <alcantarakaly@gmail.com>
2022-03-29 22:57:35 -0500

    estilos de responsive v1

--- src/view/profileRegister.js
+++ src/view/profileRegister.js
@@ -4,30 +4,35 @@ import { saveUserProfile } from '../lib/firebase/firestore.js';
 
 const profileRegister = () => {
   const viewRegister = `
-  <form class="profileRegister" id = 'profileRegister'>
-    <h2 class ='tituloProfileRegister'>Ingresa tus datos</h2>
-    <hr>
-    <img src="./img/camara.png" alt="" id='camera'>
-    <i class = "far fa-edit" id = "editPhoto"></i>
-    <p class='textCamera'>Cambiar foto de perfil</p>
-    <input type="text" id="fullName" class="fullName" placeholder="Nombre">
-    <input type="text" id="nickName" class="nickname" placeholder = "Apodo*"  requerided>
-    <input type="text" id="ocupation" class="ocupation" placeholder = "Ocupación">
-    <input type="email" id="inputemail" class="email" placeholder = "Correo electrónico" readonly >
-    <select name="gender" id="gender" class = "gender" requerided>
-        <option style = "color:gray" disabled selected>Género</option>
-        <option value="Femenino">Femenino</option>
-        <option value="Masculino">Masculino</option>
-        <option value="Prefiero no responder">Prefiero no responder</option>
-    </select>
-    <input type="number" id="age" class="age" placeholder = "Edad" min="5" max="105">
-    <input type="tel" id="phone" class="phone" placeholder = "Telefono">
-    <textarea id="introduceYourself" class="introduceYourself" placeholder = "Preséntate" cols="30" rows="5"></textarea>
-    <p class='pProfileRegister'>Aquí puedes dejar información de cómo contactarte si deseas 
-      ayudar de forma gratuita a mujeres que estén pasando por 
-      situaciones de violencia</p>
-    <p class='required'>(*)Campo obligatorio</p>
-    <input type="submit" value='Guardar'>
+  <form class="formProfileRegister" id = 'profileRegister'>
+    <div class="datos-personales">
+      <h2 class ='tituloProfileRegister'>Ingresa tus datos</h2>
+      <hr>
+      <img src="./img/camara.png" alt="" id='camera'>
+      <i class = "far fa-edit" id = "editPhoto"></i>
+      <p class='textCamera'>Cambiar foto de perfil</p>
+      <input type="text" id="fullName" class="fullName" placeholder="Nombre">
+      <input type="text" id="nickName" class="nickname" placeholder = "Apodo*"  requerided>
+    </div>
+    <div class="otros-datos">
+      <input type="text" id="ocupation" class="ocupation" placeholder = "Ocupación">
+      <input type="email" id="inputemail" class="email" placeholder = "Correo electrónico" readonly >
+      <select name="gender" id="gender" class = "gender" requerided>
+          <option style = "color:gray" disabled selected>Género</option>
+          <option value="Femenino">Femenino</option>
+          <option value="Masculino">Masculino</option>
+          <option value="Prefiero no responder">Prefiero no responder</option>
+      </select>
+      <input type="number" id="age" class="age" placeholder = "Edad" min="5" max="105">
+      <input type="tel" id="phone" class="phone" placeholder = "Telefono">
+      <textarea id="introduceYourself" class="introduceYourself" placeholder = "Preséntate" cols="30" rows="5"></textarea>
+      <p class='pProfileRegister'>Aquí puedes dejar información de cómo contactarte si deseas 
+        ayudar de forma gratuita a mujeres que estén pasando por 
+        situaciones de violencia</p>
+      <p class='required'>(*)Campo obligatorio</p>
+      <input type="submit" value='Guardar'>
+    </div>
+    
   </form>
 `;
   document.body.style.background = '#EAC9E2';
